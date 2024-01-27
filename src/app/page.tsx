32b019af2cb482a2daf8773f2209a542cb2f3a94
Swamyrangareddy Muthumula <127617789+swamyrangareddy@users.noreply.github.com>
2024-01-27 12:04:25 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         </div>
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Health Monitoring System</h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">monitor your health </p>
+          <p className="mt-6 text-lg leading-8 text-gray-600">Monitor Your Health </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link href="/login" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</Link>
             
